e3babffd5cfce9e59731774650b476a74e036ec4
Longboye99 <144575154+Longboye99@users.noreply.github.com>
2023-09-17 11:41:47 +0700

    instantiate ball prefab in GameManager

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,9 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum BallColor
+{
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
+
 public class Ball : MonoBehaviour
 {
     [SerializeField] private int point;
+    [SerializeField] private BallColor ballColor;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int playerScore = 0;
+
+    [SerializeField] private GameObject ballPrefab;
+
     public static GameManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        Instantiate(ballPrefab, Vector3.zero, Quaternion.identity);
     }
 
     // Update is called once per frame
